76bdedb66fca30259540725381a81c07f92fb6ae
dlucioyauh <dlucio.douglas@gmail.com>
2025-02-23 23:07:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jcarlo-app
---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <></> // Aqui pode vir o conteúdo da tela inicial
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <></> // Aqui pode vir o conteúdo da tela de detalhes
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
